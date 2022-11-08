da8c803cc2791d6623de5755ae6e5c9c9fc5e48c
Slavo Kozar <slavo.kozar@gmail.com>
2022-11-08 14:35:25 +0100

    lokalne zmeny

---
+++ src/pages/lekcie.js
@@ -0,0 +1,34 @@
+const lekcie = [
+    {
+        topic: 'Úvod do Webu a Reactu',
+        perex: '',
+        links: []
+    },
+    {
+        topic: 'Props a State',
+        perex: '',
+        links: []
+    },
+    {
+        topic: 'Formuláre, nekontrolované a kontrolované komponenty',
+        perex: '',
+        links: []
+    },
+    {
+        topic: 'Zoznamy, práca s poliami dát',
+        perex: '',
+        links: []
+    },
+    {
+        topic: 'Asynchrónne načítavanie dát - AJAX',
+        perex: '',
+        links: []
+    },
+    {
+        topic: 'Distribúcia stavu komponenty - Lifting state up',
+        perex: '',
+        links: []
+    },
+]
+
+export default lekcie;
