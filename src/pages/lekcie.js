24499861f6559b92534c761837f3400b5f5ac634
Slavo Kozar <slavo.kozar@gmail.com>
2022-11-08 15:08:30 +0100

    upravena stranka lekcii

--- src/pages/lekcie.js
+++ src/pages/lekcie.js
@@ -2,12 +2,30 @@ const lekcie = [
     {
         topic: 'Úvod do Webu a Reactu',
         perex: '',
-        links: []
+        links: [
+            {
+                href: `prezentacia_lekcia1.pdf`,
+                label: 'Prezentácia'
+            },
+            {
+                href: `lekcia1`,
+                label: 'Zdroje pre lekciu'
+            }
+        ]
     },
     {
         topic: 'Props a State',
         perex: '',
-        links: []
+        links: [
+            {
+                href: `prezentacia_lekcia2.pdf`,
+                label: 'Prezentácia'
+            },
+            {
+                href: `lekcia2`,
+                label: 'Zdroje pre lekciu'
+            }
+        ]
     },
     {
         topic: 'Formuláre, nekontrolované a kontrolované komponenty',
